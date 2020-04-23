39aed9e76612e874c55643495127c3de6624084b
ethan <graf.ethan@gmail.com>
2020-04-22 23:48:33 -0400

    Basic executor and messenger

--- main.go
+++ main.go
@@ -1,13 +1,33 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
 	"github.com/gorilla/websocket"
 )
 
-//var synchronizerURL = os.Getenv("SYNCHRONIZER_IP")
+var connection *websocket.Conn
+var tasks chan *Payload = make(chan *Payload, 1024)
+
+type TaskMessage struct {
+	MessageType string
+	Payload     Payload
+}
+
+type Payload struct {
+	IntentType string
+	TaskType   string
+	TaskUUID   string
+	Config     interface{}
+	Input      Input
+}
+
+type Input struct {
+	Board []int8 `json:"board"`
+	Size  int    `json:"size"`
+}
 
 func main() {
 	connection, err := registerWorker()
@@ -16,6 +36,7 @@ func main() {
 		return
 	}
 	go listen(connection)
+	go execute(tasks, connection)
 
 	select {}
 }
@@ -38,5 +59,97 @@ func listen(connection *websocket.Conn) {
 			return
 		}
 		fmt.Println(string(buffer))
+
+		// Try to read message into JSON
+		//		var message map[string]interface{}
+		var message TaskMessage
+		err = json.Unmarshal(buffer, &message)
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println(message)
+
+		if message.MessageType == "Intent" {
+			payload := message.Payload
+
+			tasks <- &payload
+		}
 	}
+}
+
+func sendMessage(message *map[string]interface{}, connection *websocket.Conn) {
+	go func() {
+		// Convert to buffer
+		buffer, err := json.Marshal(message)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		connection.WriteMessage(websocket.TextMessage, buffer)
+	}()
+}
+
+func execute(tasks chan *Payload, connection *websocket.Conn) {
+	for {
+		task := <-tasks
+		fmt.Println("hello")
+
+		if task.TaskType == "GOL" {
+			sendMessage(&map[string]interface{}{
+				"MessageType": "WorkResponse",
+				"Output":      gol(task.Input.Size, task.Input.Board),
+				"start":       1,
+				"end":         2,
+			}, connection)
+
+		} else {
+			fmt.Println("Unknown task")
+		}
+	}
+}
+
+func gol(size int, board []int8) []int8 {
+	result := make([]int8, size*size)
+	for i := 0; i < size*size; i++ {
+		x := i % size
+		y := i / size
+
+		x1 := (x + size - 1) % size
+		x2 := x
+		x3 := (x + size + 1) % size
+
+		y1 := ((y + size - 1) % size) * size
+		y2 := y * size
+		y3 := ((y + size + 1) % size) * size
+
+		var count int8 = 0
+
+		count += board[x1+y1]
+		count += board[x2+y1]
+		count += board[x3+y1]
+
+		count += board[x1+y2]
+		count += board[x3+y2]
+
+		count += board[x1+y3]
+		count += board[x2+y3]
+		count += board[x3+y3]
+
+		if board[x2+y2] == 1 {
+			if count < 2 {
+				result[x2+y2] = 0
+			} else if count > 3 {
+				result[x2+y2] = 0
+			} else {
+				result[x2+y2] = 1
+			}
+		} else {
+			if count == 3 {
+				result[x2+y2] = 1
+			} else {
+				result[x2+y2] = 0
+			}
+		}
+	}
+	return result
 }
